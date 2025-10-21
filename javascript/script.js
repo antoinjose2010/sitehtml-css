d751541d96a17af61bbea63419780c1ec6a29673
unknown <lucaslemoss608@gmail.com>
2025-10-21 08:22:41 -0300

    introdução ao js

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+// QUESTÃO 1 
+let n1 = parseFloat(prompt);
+let n2 = parseFloat(prompt);
+if (n1 > n2) {
+    alert(n1 , "è maior")
+}
+if (n2 < n1) {
+    alert(n2 , "è maior")   
+}
+if (n1 == n2) {
+    alert(n1 ,"e" , n2 , "são iguais")       
+}
