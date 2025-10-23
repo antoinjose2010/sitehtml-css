3724c45271cfa6a48a7134e7b2c5aabc824c1d69
unknown <lucaslemoss608@gmail.com>
2025-10-23 14:11:13 -0300

    fazendo questo~es em js

--- javascript2/script.js
+++ javascript2/script.js
@@ -1,11 +1,49 @@
 // 1 - Faça um algoritmo que leia os valores de A, B, C e em seguida imprima na tela a soma entre A e B é mostre se a soma é menor que C.
-let a = parseFloat(prompt("Digite o Primeiro valor:  "))
-let b = parseFloat(prompt("Digite o Segundo valor:  "))
-let c = parseFloat(prompt("Digite o Terceiro valor:  "))
-let soma = (a+b)
-if (soma < c ) {
-    alert( soma + "   é menor que:   " +  c )
-}
-if (soma > c) {
-    alert( soma + "   é maior que:   " +  c )
-}
+
+// let a = parseFloat(prompt("Digite o Primeiro valor:  "))
+// let b = parseFloat(prompt("Digite o Segundo valor:  "))
+// let c = parseFloat(prompt("Digite o Terceiro valor:  "))
+// let soma = (a+b)
+// if (soma < c ) {
+//     alert( soma + "   é menor que:   " +  c )
+// }
+// if (soma > c) {
+//     alert( soma + "   é maior que:   " +  c )
+// }
+
+// 2 - Faça um algoritmo para receber um número qualquer e imprimir na tela se o número é par ou ímpar, positivo ou negativo
+
+// let n = parseFloat(prompt("Digite um número e lhe direi se é impar ou par:  "))
+// if (n % 2 == 0) {
+//     alert(n + "  é par")
+// }
+// else {
+//     alert(n + "  é impar")
+// }
+
+//  11 - Faça um algoritmo que leia quatro notas obtidas por um aluno, calcule a média das nota obtidas, imprima na tela o nome do aluno e 
+//  se o aluno foi aprovado ou reprovado. Para o aluno ser considerado aprovado sua média final deve ser maior ou igual a 7.
+
+// let soma = 0
+// for (let c = 4; c > 0; c -= 1) {
+//     let nota = parseFloat(prompt("Digite a " + c + " nota:  "))
+//     nota += soma
+// }
+// let media = soma / 4
+// if (media >= 7) {
+//     alert("Aluno aprovado")
+// }
+// else {
+//     alert("Aluno reprovado")
+// }
+
+//  10 - Faça um algoritmo que leia três notas obtidas por um aluno, e imprima na tela a média das notas.
+
+// let soma = 0
+// for (let c = 1; c <= 3; c++) {
+//     let nota = parseFloat(prompt("Digite a " + c + "  nota do aluno:"))
+//     soma += nota
+// }
+// let media = soma / 3
+// alert("Sua média é: " + media)
+
